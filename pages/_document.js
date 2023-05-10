69a95d829e4cb5c84b8088aa6bcabc5bc2d53b1c
Jason <jasonygodfrey@gmail.com>
2023-05-10 16:32:02 -0700

    hotjar

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,18 @@ class MyDocument extends Document {
     return (
       <Html lang="zxx">
         <Head>
+          {/* Add your Hotjar script here */}
+          <script>
+            {`(function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3486958,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+          </script>
+
           <link
             href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap"
             rel="stylesheet"
